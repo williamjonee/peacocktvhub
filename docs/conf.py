c6baae5840a265dde3e614992defc43029f9c2d7
williamjonee <williamjone07@outlook.com>
2025-07-01 12:53:09 +0530

    Create conf.py

---
+++ docs/conf.py
@@ -0,0 +1,10 @@
+project = 'peacocktvhub'
+author = 'Your Name'
+release = '1.0'
+
+extensions = []
+templates_path = ['_templates']
+exclude_patterns = []
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
